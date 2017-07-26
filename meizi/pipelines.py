c647cb3a64f9b0a2ac3eaa9392b8801c6d1e21d9
lijing01 <tracelijing@gmail.com>
2017-07-26 22:31:06 +0800

    Signed-off-by: lijing01 <tracelijing@gmail.com>

---
+++ meizi/pipelines.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+import os
+import requests
+from meizi import settings
+from scrapy.pipelines.images import ImagesPipeline
+import scrapy
+from scrapy.exceptions import DropItem
+
+class MeiziPipeline(object):
+    def process_item(self, item, spider):
+        return item
+
+
+class ImageDownloadPipeline(ImagesPipeline):
+    default_headers = {
+        'Host': 'mm.howkuai.com',
+        'Connection': 'keep - alive',
+        'Upgrade - Insecure - Requests': '1',
+        'User - Agent': 'Mozilla / 5.0(Windows NT 10.0;Win64; x64) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 58.0 .3029.110 Safari / 537.36',
+        'Accept': 'text / html, application / xhtml + xml, application / xml;q = 0.9, image / webp, * / *;q = 0.8',
+        'Accept - Encoding': 'gzip, deflate, sdch',
+    }
+
+    def get_media_requests(self, item, info):
+        for image_url in item['image_urls']:
+            yield scrapy.Request(image_url,headers = self.default_headers)
+
+    def item_completed(self, results, item, info):
+        image_paths = [x['path'] for ok, x in results if ok]
+        if not image_paths:
+            raise DropItem("Item contains no images")
+        item['image_paths'] = image_paths
+        return item
